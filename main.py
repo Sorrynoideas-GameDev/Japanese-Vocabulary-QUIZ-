40530e7845d5f892fc5ee935cd212e783976f5c5
adwayjha16 <adway.24bce10241@vitbhopal.ac.in>
2026-04-14 18:16:03 +0530

    j

--- main.py
+++ main.py
@@ -12,17 +12,6 @@ Orchestrates the flow between screens:
 Also manages loading/saving of vocabulary and progress data.
 """
 
-
-
-
-
-
-
-
-
-
-
-
 import os
 import sys
 from data_loader import load_vocabulary, load_progress, save_progress
